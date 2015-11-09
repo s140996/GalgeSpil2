cc875d9721c5dce7433953c9e9f2da3954dda10d
SteenBartholdy <s145090@student.dtu.dk>
2015-11-09 11:47:05 +0100

    Det for vildt

--- app/src/main/java/thostrup/dk/galgespil2/StartScreen.java
+++ app/src/main/java/thostrup/dk/galgespil2/StartScreen.java
@@ -1,7 +1,9 @@
 package thostrup.dk.galgespil2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        findViewById(R.id.buttonNew).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartScreen.this, GalgeSpil.class));
+            }
+        });
+        findViewById(R.id.buttonIndstillinger).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartScreen.this, Indstillinger.class));
+            }
+        });
+        findViewById(R.id.buttonHelp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartScreen.this, Help.class));
+            }
+        });
     }
 }
