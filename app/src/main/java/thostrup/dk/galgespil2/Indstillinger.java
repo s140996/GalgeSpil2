ba7642bf769032aa6adfaf20a60d713b3b9a064b
Roos <s145094@student.dtu.dk>
2015-11-10 18:59:58 +0100

    Ordliste sortering

--- app/src/main/java/thostrup/dk/galgespil2/Indstillinger.java
+++ app/src/main/java/thostrup/dk/galgespil2/Indstillinger.java
@@ -2,12 +2,20 @@ package thostrup.dk.galgespil2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class Indstillinger extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Indstillinger extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView list;
+    private ArrayList<String> ord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,17 @@ public class Indstillinger extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.listView);
 
-        list.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,  android.R.id.text1, StartScreen.game.getMuligeOrd()));
+        ord = StartScreen.game.getMuligeOrd();
+
+        Collections.sort(ord, String.CASE_INSENSITIVE_ORDER);
+
+        list.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, ord));
+
+        list.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), "Valgt ord: " + list.getItemAtPosition(position), Toast.LENGTH_LONG).show();
     }
 }
